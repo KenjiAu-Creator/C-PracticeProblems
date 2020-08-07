fcb33528b3e41ae09a95a7018d6b4d1ec9836ec4
Kenji <kenji.au9@gmail.com>
2020-08-06 23:52:09 -0600

    Added in uppercase problem. Started IntSort

---
+++ Capitalize/Capitalize/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Capitalize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This program will take in a string and convert it to uppercase
+            Console.WriteLine("Input a string to convert to uppercase");
+            string word = Console.ReadLine();
+            string value = word.ToUpper();
+            Console.WriteLine(value);
+        }
+    }
+}
---
+++ IntegerSort/IntegerSort/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+namespace IntegerSort
+{
+    class Program
+    {
+        public static void Sort(Array array, int index, int length);
+
+        static void Main(string[] args)
+        {
+            // This program will take in three integers, and determin the highest and lowest of the three
+            // 
+
+            // Int 1
+            Console.WriteLine("Input integer 1");
+            int integer1 = Convert.ToInt32(Console.ReadLine());
+
+            // Int 2
+            Console.WriteLine("Input integer 2");
+            int integer2 = Convert.ToInt32(Console.ReadLine());
+
+            // Int 3
+            Console.WriteLine("Input integer 3");
+            int integer3 = Convert.ToInt32(Console.ReadLine());
+
+            int[] numbers = {integer1, integer2, integer3 };
+
+            Array.Sort.(numbers, 0, 3);
+
+            DisplayValues(numbers);
+        }
+    }
+}
