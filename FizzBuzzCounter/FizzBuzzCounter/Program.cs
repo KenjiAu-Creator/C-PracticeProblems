cb304bfe9627430b0056bb0d20e5dddd5f4b8638
Kenji <kenji.au9@gmail.com>
2020-08-08 15:36:37 -0600

    Added in FizzBuzz

---
+++ FizzBuzzCounter/FizzBuzzCounter/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace FizzBuzzCounter
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // This program will count from 1 to 100.
+      // Every number divisible by 3 will be replaced with "Fizz"
+      // Every number divisible by 5 will be replaced with "Buzz"
+      // Every number divisible by both 3 and 5 will be replaced with "FizzBuzz"
+      // This is by extension -> divisible by 15, ( 3 * 5 )
+
+      // Initialize variables
+      int fizz = 3;
+      int buzz = 5;
+      int fizzBuzz = 15;
+
+      // Loop
+      for (int i = 1; i <= 100; i++)
+      {
+        if (i % fizzBuzz == 0)
+        {
+          Console.WriteLine("FizzBuzz");
+        }
+        else if (i % buzz == 0)
+        {
+          Console.WriteLine("Buzz");
+        }
+        else if (i % fizz == 0)
+        {
+          Console.WriteLine("Fizz");
+        }
+        else
+          Console.WriteLine(i);
+      }
+    }
+  }
+}
