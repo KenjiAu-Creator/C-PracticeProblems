8a1f59beafd26d9516f8431c35cd70f65b806ee4
Kenji <kenji.au9@gmail.com>
2020-08-08 13:44:35 -0600

    Finished simpleCalc

---
+++ SimpleCalc/SimpleCalc/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace SimpleCalc
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // This program will take in an operator (Such as + or *) and two operands (as ints) and output the result.
+      // Ensure that division produces accurate results -> decimals and therefore the answer is not of type int
+      
+      // Input operator
+      Console.WriteLine("What type of operation would you like to do? (+, - , *, / )");
+      string op = Console.ReadLine();
+
+      // Input int 1
+      Console.WriteLine("Enter in integer 1:");
+      int integer1 = Convert.ToInt32(Console.ReadLine());
+
+      // Input int 2
+      Console.WriteLine("Enter in integer 2:");
+      int integer2 = Convert.ToInt32(Console.ReadLine());
+
+      // Actual calculation
+
+      switch (op)
+      {
+        case ("+"):
+          int result = (integer1 + integer2);
+          Console.WriteLine("The resultant is " + result.ToString());
+          break;
+        case ("-"):
+          result = (integer1 - integer2);
+          Console.WriteLine("The resultant is " + result.ToString());
+          break;
+        case ("*"):
+          result = (integer1 * integer2);
+          Console.WriteLine("The resultant is " + result.ToString());
+          break;
+        case ("/"):
+          double resultant = Math.Round((Convert.ToDouble(integer1) / Convert.ToDouble(integer2)),3);
+          Console.WriteLine("The resultant is " + resultant.ToString());
+          break;
+        default:
+          Console.WriteLine("Sorry this calculator does not support that operation");
+          break;
+      }
+    }
+  }
+}
