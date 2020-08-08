7fcf75a171ea80081ac20a29318530ecf0b09d92
Kenji <kenji.au9@gmail.com>
2020-08-08 14:55:46 -0600

    Added in DoWhileHello

---
+++ DoWhileHello/DoWhileHello/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DoWhileHello
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // This program will say "Hello" and wait for input from the user.
+      // It will continue saying "Hello" until the user enters "stop" (case insensitive)
+      // Use a do-while loop
+
+      // Initialize
+      Boolean exit = false;
+
+      do
+      {
+        Console.WriteLine("Hello");
+        string userInput = Console.ReadLine().ToLower();
+        if (userInput == "stop")
+          {
+            exit = true;
+          }
+      }
+      while (exit == false);
+    }
+  }
+}
