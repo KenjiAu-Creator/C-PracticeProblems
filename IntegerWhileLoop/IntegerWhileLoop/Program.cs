c4d5eeb71a3dc26df02f270051530c5a5feb4959
Kenji <kenji.au9@gmail.com>
2020-08-08 16:45:36 -0600

    Added Exception handling to IntegerWhileLoop

--- IntegerWhileLoop/IntegerWhileLoop/Program.cs
+++ IntegerWhileLoop/IntegerWhileLoop/Program.cs
@@ -7,28 +7,48 @@ namespace IntegerWhileLoop
   {
     static void Main(string[] args)
     {
-      // This program will prompt the user for an integer and continue prompting until an integer between 1 and 10 is entered.
-      // Use a while loop
-
-
-      // Initialize
-      Boolean inputAcceptable = false;
-      
-      // user input
-      while (inputAcceptable != true)
+      try
       {
-        // Do stuff
-        Console.WriteLine("Please input an integer between 1 and 10.");
-        int userInput = Convert.ToInt32(Console.ReadLine());
-        if (userInput <= 10 && userInput >= 1)
+        // This program will prompt the user for an integer and continue prompting until an integer between 1 and 10 is entered.
+        // Use a while loop
+
+        // Initialize
+        Boolean inputAcceptable = false;
+
+        // user input
+        while (inputAcceptable != true)
         {
-          inputAcceptable = true;
-          Console.WriteLine("Thank you.");
+          // Do stuff
+          Console.WriteLine("Please input an integer between 1 and 10.");
+
+          string userStringInput = Console.ReadLine();
+          int userInput = 0;
+
+          int value;
+          if (int.TryParse(userStringInput, out value))
+          {
+            userInput = Convert.ToInt32(userStringInput);
+          }
+          else
+          {
+            throw new Exception("Expected a value between 1 and 10.");
+          }
+
+          if (userInput <= 10 && userInput >= 1)
+          {
+            inputAcceptable = true;
+            Console.WriteLine("Thank you.");
+          }
+          else
+          {
+            Console.WriteLine("That number is not between 1 and 10!");
+          }
         }
-        else
-        {
-          Console.WriteLine("Thats not between 1 and 10!");
-        }
+      }
+
+      catch (Exception ex)
+      {
+        Console.WriteLine(ex.Message);
       }
     }
   }
