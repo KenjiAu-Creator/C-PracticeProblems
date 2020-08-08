661fc41511708af8702077d36c9748185cff343a
Kenji <kenji.au9@gmail.com>
2020-08-07 19:44:21 -0600

    Finished code for IntegerSort

--- IntegerSort/IntegerSort/Program.cs
+++ IntegerSort/IntegerSort/Program.cs
@@ -1,34 +1,41 @@
 ﻿using System;
-
+using System.ComponentModel.DataAnnotations;
 
 namespace IntegerSort
 {
     class Program
     {
-        public static void Sort(Array array, int index, int length);
-
         static void Main(string[] args)
         {
             // This program will take in three integers, and determin the highest and lowest of the three
             // 
 
-            // Int 1
+            // Int 1 input
             Console.WriteLine("Input integer 1");
             int integer1 = Convert.ToInt32(Console.ReadLine());
 
-            // Int 2
+            // Int 2 input
             Console.WriteLine("Input integer 2");
             int integer2 = Convert.ToInt32(Console.ReadLine());
 
-            // Int 3
+            // Int 3 input
             Console.WriteLine("Input integer 3");
             int integer3 = Convert.ToInt32(Console.ReadLine());
 
-            int[] numbers = {integer1, integer2, integer3 };
+            // Step 1. This step determines a local maximum
+            int max1 = Math.Max(integer1, integer2);
 
-            Array.Sort.(numbers, 0, 3);
+            // Step 2. This step determines the absolute maximum
+            int highest = Math.Max(max1, integer3);
 
-            DisplayValues(numbers);
+            // Step 3. This step determines a local minimum
+            int min1 = Math.Min(integer1, integer2);
+
+            // Step 4. This step determines the absolute minimum
+            int lowest = Math.Min(min1, integer3);
+
+            Console.WriteLine("The highest value is " + highest);
+            Console.WriteLine("The lowest value is " + lowest);
         }
     }
 }
