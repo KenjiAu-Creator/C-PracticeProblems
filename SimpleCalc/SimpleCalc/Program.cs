0fc2d86f152b2d2e888ab4211f1053bfb79830ff
Kenji <kenji.au9@gmail.com>
2020-08-08 16:57:59 -0600

    Added in Validation of divide 0 in simpleCalc

--- SimpleCalc/SimpleCalc/Program.cs
+++ SimpleCalc/SimpleCalc/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq.Expressions;
 
 namespace SimpleCalc
 {
@@ -12,7 +13,7 @@ namespace SimpleCalc
       // Input operator
       Console.WriteLine("What type of operation would you like to do? (+, - , *, / )");
       string op = Console.ReadLine();
-
+      
       // Input int 1
       Console.WriteLine("Enter in integer 1:");
       int integer1 = Convert.ToInt32(Console.ReadLine());
@@ -38,13 +39,18 @@ namespace SimpleCalc
           Console.WriteLine("The resultant is " + result.ToString());
           break;
         case ("/"):
-          double resultant = Math.Round((Convert.ToDouble(integer1) / Convert.ToDouble(integer2)),3);
+          while (integer2 == 0)
+            {
+              Console.WriteLine("You can not divide by 0! Please enter in a new divisor");
+              integer2 = Convert.ToInt32(Console.ReadLine());
+            }
+          double resultant = Math.Round((Convert.ToDouble(integer1) / Convert.ToDouble(integer2)), 3);
           Console.WriteLine("The resultant is " + resultant.ToString());
           break;
         default:
           Console.WriteLine("Sorry this calculator does not support that operation");
           break;
+        }
       }
     }
   }
-}
